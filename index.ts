ef9744a1191cdf85e25821a60fc0908bbd290255
ggilmore <geoffrey@sourcegraph.com>
2019-04-26 13:55:19 -0700

    move values.yaml to separate file

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs'
 import * as os from 'os'
 import * as path from 'path'
 
@@ -71,104 +72,11 @@ const clusterAdmin = new k8s.rbac.v1.ClusterRoleBinding(
 //     .getResource('v1/Service', 'ingress-nginx', 'ingress-nginx')
 //     .apply(svc => svc.status.apply(status => status.loadBalancer.ingress.map(i => i.ip)))
 
-const values = yaml.safeLoad(`
-cluster:
-  frontend:
-    containers:
-      frontend:
-        limits:
-          cpu: "1"
-        requests:
-          cpu: "1"
-    nodeSelector:
-      beta.kubernetes.io/instance-type: m4.4xlarge
-    replicas: 2
-  githubProxy:
-    nodeSelector:
-      beta.kubernetes.io/instance-type: m4.4xlarge
-  gitserver:
-    containers:
-      gitserver:
-        limits:
-          cpu: "8"
-          memory: 8Gi
-        requests:
-          cpu: "8"
-          memory: 8Gi
-    diskSize: 200Gi
-    nodeSelector:
-      beta.kubernetes.io/instance-type: i3.4xlarge
-    shards: 3
-  indexedSearch:
-    diskSize: 600Gi
-    nodeSelector:
-      beta.kubernetes.io/instance-type: m4.4xlarge
-  lspProxy:
-    nodeSelector:
-      beta.kubernetes.io/instance-type: i3.4xlarge
-  searcher:
-    nodeSelector:
-      beta.kubernetes.io/instance-type: i3.4xlarge
-    replicas: 7
-  storageClass:
-    create: none
-    name: standard
-  symbols:
-    replicas: 4
-  xlangGo:
-    nodeSelector:
-      beta.kubernetes.io/instance-type: i3.4xlarge
-    replicas: 2
-  xlangJava:
-    nodeSelector:
-      beta.kubernetes.io/instance-type: i3.4xlarge
-    replicas: 2
-  xlangJavascriptTypescript:
-    nodeSelector:
-      beta.kubernetes.io/instance-type: i3.4xlarge
-  xlangPHP:
-    nodeSelector:
-      beta.kubernetes.io/instance-type: i3.4xlarge
-  xlangPython:
-    nodeSelector:
-      beta.kubernetes.io/instance-type: i3.4xlarge
-    replicas: 4
-site:
-  appURL: https://sourcegraph.lyft.net
-  auth.provider: saml
-  auth.saml:
-    identityProviderMetadataURL: http://saml-metadata/saml-metadata.xml
-    serviceProviderCertificate: REDACTED
-    serviceProviderPrivateKey: REDACTED
-    type: ""
-  experimentalFeatures: {}
-  gitMaxConcurrentClones: 3
-  github:
-  - initialRepositoryEnablement: true
-    token: REDACTED
-    url: https://github.com
-  httpNodePort: 30080
-  langservers:
-  - language: go
-  - language: javascript
-  - language: typescript
-  - initializationOptions:
-      pipArgs:
-      - --index-url=https://pypi.lyft.net/pypi/
-    language: python
-  - language: java
-  - language: php
-  maxReposToSearch: 999999
-  repoListUpdateInterval: 15
-  siteID: "123123123123"
-  update.channel: release
-`)
-
 const sourcegraph = new k8s.helm.v2.Chart(
     'sourcegraph',
     {
         chart: 'https://github.com/sourcegraph/deploy-sourcegraph/archive/v2.13.5-helm.tar.gz',
-        values,
+        values: yaml.safeLoad(fs.readFileSync('values.yaml').toString()),
     },
     {
         providers: { kubernetes: k8sProvider },
